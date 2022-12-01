95b952f69c951aec8182d7730815a74581ffb021
vladbv16 <117750736+vladbv16@users.noreply.github.com>
2022-12-01 13:22:53 +0400

    Added create, edit function for main table

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
 
   // получение списка пользователей
 app.get("/", function(req, res){
-    pool.query('select Дата, филиалы.адрес as "Филиал", клиенты.фамилия as "Фамилия_клиента", сотрудники.фамилия as "Фамилия_сотрудника", `каталог услуг`.наименование, `каталог услуг`.цена from `учет оказания услуг` join сотрудники on `учет оказания услуг`.id_сотрудник = сотрудники.id join клиенты on `учет оказания услуг`.id_клиент = клиенты.id join филиалы on `учет оказания услуг`.id_филиал = филиалы.id join `каталог услуг` on `учет оказания услуг`.id_услуга = `каталог услуг`.id', function(err, data) {
+    pool.query('select `учет оказания услуг`.id, Дата, филиалы.адрес as "Филиал", клиенты.фамилия as "Фамилия_клиента", сотрудники.фамилия as "Фамилия_сотрудника", `каталог услуг`.наименование, `каталог услуг`.цена from `учет оказания услуг` join сотрудники on `учет оказания услуг`.id_сотрудник = сотрудники.id join клиенты on `учет оказания услуг`.id_клиент = клиенты.id join филиалы on `учет оказания услуг`.id_филиал = филиалы.id join `каталог услуг` on `учет оказания услуг`.id_услуга = `каталог услуг`.id', function(err, data) {
       if(err) return console.log(err);
       res.render("index.hbs", {
           all: data
@@ -34,7 +34,7 @@ app.get("/services", function(req, res){
 });
 
 app.get("/branches", function(req, res){
-    pool.query('select филиалы.Адрес, филиалы.Телефон, сотрудники.Фамилия from `филиалы` join сотрудники on `филиалы`.id_администратор = сотрудники.id', function(err, data) {
+    pool.query('select филиалы.id, филиалы.Адрес, филиалы.Телефон, сотрудники.Фамилия from `филиалы` join сотрудники on `филиалы`.id_администратор = сотрудники.id', function(err, data) {
       if(err) return console.log(err);
       res.render("branches.hbs", {
           all: data
@@ -43,7 +43,7 @@ app.get("/branches", function(req, res){
 });
 
 app.get("/employers", function(req, res){
-    pool.query('select Фамилия, Имя, Телефон, Оклад, Адрес, Название from `сотрудники` join должности on `сотрудники`.id_должность = должности.id', function(err, data) {
+    pool.query('select сотрудники.id, Фамилия, Имя, Телефон, Оклад, Адрес, Название from `сотрудники` join должности on `сотрудники`.id_должность = должности.id', function(err, data) {
       if(err) return console.log(err);
       res.render("employers.hbs", {
           all: data
@@ -52,7 +52,7 @@ app.get("/employers", function(req, res){
 });
 
 app.get("/jobtitle", function(req, res){
-    pool.query('select Название, `График работы` as график from должности', function(err, data) {
+    pool.query('select id, Название, `График работы` as график from должности', function(err, data) {
       if(err) return console.log(err);
       res.render("jobtittle.hbs", {
           all: data
@@ -85,40 +85,43 @@ app.post("/create", urlencodedParser, function (req, res) {
     });
 });
  
-// // получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
-// app.get("/edit/:id", function(req, res){
-//   const id = req.params.id;
-//   pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
-//     if(err) return console.log(err);
-//      res.render("edit.hbs", {
-//         user: data[0]
-//     });
-//   });
-// });
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  pool.query("SELECT * FROM `учет оказания услуг` WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit.hbs", {
+        user: data[0]
+    });
+  });
+});
 
-// // получаем отредактированные данные и отправляем их в БД
-// app.post("/edit", urlencodedParser, function (req, res) {
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, function (req, res) {
          
-//   if(!req.body) return res.sendStatus(400);
-//   const name = req.body.name;
-//   const age = req.body.age;
-//   const id = req.body.id;
+  if(!req.body) return res.sendStatus(400);
+  const date = req.body.date;
+  const service = req.body.service;
+  const employer = req.body.employer;
+  const customer = req.body.customer;
+  const branch = req.body.branch;
+  const id = req.body.id;
    
-//   pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
-//     if(err) return console.log(err);
-//     res.redirect("/");
-//   });
-// });
+  pool.query("UPDATE `Учет оказания услуг` SET Дата=?, id_услуга=?, id_сотрудник=?, id_клиент=?, id_филиал=? WHERE id=?", [date, service, employer, customer, branch, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
  
-// // получаем id удаляемого пользователя и удаляем его из бд
-// app.post("/delete/:id", function(req, res){
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
           
-//   const id = req.params.id;
-//   pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
-//     if(err) return console.log(err);
-//     res.redirect("/");
-//   });
-// });
+  const id = req.params.id;
+  pool.query("DELETE FROM `учет оказания услуг` WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
  
 app.listen(3000, function(){
   console.log("Сервер ожидает подключения...");
